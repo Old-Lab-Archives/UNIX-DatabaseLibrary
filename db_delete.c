d51a9550027adca59633f176a6e66c3d2cea5bc4
Aswini S <ashumeow@live.com>
2014-05-23 09:03:33 +0530

    Create db_delete.c

---
+++ db_delete.c
@@ -0,0 +1,20 @@
+#include "SugarDB.h"
+
+/* Deleting the specified record */
+int db_delete(DB *db, const char *key)
+{
+	int rc;
+	if(_db_find(db, key, 1) == 0)
+	{
+		rc = _db_dodelete(db); /* record found */
+		db->cnt_delok++;
+	}
+	else
+	{
+		rc = -1; /* not found */
+		db->cnt_delerr++;
+	}
+	if(un_lock(db->idxfd, db->chainoff, SEEK_SET, 1) < 0)
+		err_dump("error");
+	return(rc);
+}
